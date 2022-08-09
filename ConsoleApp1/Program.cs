6c9659d46032ad32c24600cd8c31322c51531652
Pavel A. Bandaletov <bandaletov72@gmail.com>
2022-08-09 13:28:47 +0300

    length

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = "abcdefg";
+Console.WriteLine("1) The length of '{0}' is {1}", str, str.Length);
+Console.WriteLine("2) The length of '{0}' is {1}", "xyz", "xyz".Length);
+
+int length = str.Length;
+Console.WriteLine("3) The length of '{0}' is {1}", str, length);
 Console.ReadLine();
